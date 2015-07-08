ef2746834a7d6f08fe61c231489c94e7cb1da6f3
Alberto Villalobos <alberto@albertovillalobos.me>
2015-07-07 17:50:02 -0700

    got the list of movies working again!

--- movielist.js
+++ movielist.js
@@ -6,6 +6,7 @@ var {
   Text,
   TextInput,
   View,
+  Image,
 } = React;
 
 // var TimerMixin = require('react-timer-mixin');
@@ -16,12 +17,123 @@ var {
 
 
 var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+// var REQUEST_URL = 'http://haxw.me/api/users/';
 var LOADING = {};
 
 var movielist = React.createClass({
-  // mixins: [TimerMixin],
-  // timeoutID: (null: any),
-})
+  getInitialState: function() {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  },
+
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
+        });
+      })
+      .done();
+  },
+
+
+
+  render: function() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie}
+        style={styles.listView}
+      />
+  );
+
+   },
+
+
+   renderLoadingView: function() {
+     return (
+       <View style={styles.container}>
+         <Text>
+           Loading users...
+         </Text>
+       </View>
+     );
+   },
+
+   renderMovie: function(movie) {
+     return (
+       <View style={styles.container}>
+         <Image
+           source={{uri: movie.posters.thumbnail}}
+           style={styles.thumbnail}
+         />
+         <View style={styles.rightContainer}>
+           <Text style={styles.title}>{movie.title}</Text>
+           <Text style={styles.year}>{movie.year}</Text>
+         </View>
+       </View>
+     );
+   },
+
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  thumbnail: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    padding: 10,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
+
+
+});
 
 
 module.exports = movielist;
